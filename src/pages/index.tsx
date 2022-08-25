233d081b5f39ebf7a5334592a1b61fd2adddb9ae
Maxel-Uds <maxellopes32@gmail.com>
2022-08-24 23:18:11 -0300

    Componente Tabela parte 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Max', 22, '1'),
+    new Cliente('João', 32, '2')
+  ]
+
   return (
     <div className={`
       flex h-screen 
@@ -9,7 +17,7 @@ export default function Home() {
       from-blue-500 to-purple-500 
       text-white`}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>    
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: [
+  content: [
       './src/pages/**/*.{js,ts,jsx,tsx}',
       './src/components/**/*.{js,ts,jsx,tsx}'
-    ],
-    safelist: [
-      /^bg-/,
-      /^to-/,
-      /^from-/,
-    ]
-  },
+  ],
+  safelist: [
+      'bg-',
+      'to-',
+      'from-',
+  ],
   theme: {
     extend: {},
   },
